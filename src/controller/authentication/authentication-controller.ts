343943b35222e65fa632e40d4f37895b5d5f3774
Preethishjaintr <preethishjain03@gmail.com>
2025-03-28 11:09:10 +0530

    add jsonwebtoken dependency and environment configuration for JWT secret key

--- src/controller/authentication/authentication-controller.ts
+++ src/controller/authentication/authentication-controller.ts
@@ -4,7 +4,7 @@ import {
   SignUpWithUsernameAndPasswordError,
   type LogInWithUsernameAndPasswordResult,
   type SignUpWithUsernameAndPasswordResult,
-} from "./authentication-types";
+} from "./authentication-types.js";
 import { prismaClient } from "../../extras/prisma";
 import jwt from "jsonwebtoken";
 import { jwtSecretKey } from "../../environment";
